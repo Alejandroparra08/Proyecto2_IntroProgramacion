e9572bf3952ca92719f8744f4f692294c316dd57
Alejandroparra08 <alejandroparrarojas6@gmail.com>
2026-06-11 12:10:53 -0600

    Add files via upload
    Agregar menú principal

--- main.py
+++ main.py
@@ -1,15 +1,37 @@
 import tkinter as tk
 
+def registrarse():
+    print("Ventana de registro")
+
+def iniciar_sesion():
+    print("Ventana de inicio de sesión")
+
 ventana = tk.Tk()
+
 ventana.title("Defensa y Asalto de Base")
 ventana.geometry("800x600")
 
-etiqueta = tk.Label(
+titulo = tk.Label(
     ventana,
-    text="Proyecto Defensa y Asalto de Base"
+    text="Defensa y Asalto de Base",
+    font=("Arial", 20)
 )
+titulo.pack(pady=30)
 
-etiqueta.pack(pady=20)
+boton_registro = tk.Button(
+    ventana,
+    text="Registrarse",
+    width=20,
+    command=registrarse
+)
+boton_registro.pack(pady=10)
+
+boton_login = tk.Button(
+    ventana,
+    text="Iniciar Sesión",
+    width=20,
+    command=iniciar_sesion
+)
+boton_login.pack(pady=10)
 
 ventana.mainloop()
-
